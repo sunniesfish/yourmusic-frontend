bb1f1c877e0967cd37fa1a3fec9ec32fc3154e13
sunniesfish <hhch5611.jh@gmail.com>
2025-04-26 16:57:54 +0900

    ssr test

--- src/app/playlists/[playlist-id]/layout.tsx
+++ src/app/playlists/[playlist-id]/layout.tsx
@@ -1,14 +1,9 @@
-"use client";
-import ApolloProvider from "@/providers/apollo-provider";
-
 export default function PlaylistDetailLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ApolloProvider>
-      <main className="container mx-auto px-4 py-8 space-y-6">{children}</main>
-    </ApolloProvider>
+    <main className="container mx-auto px-4 py-8 space-y-6">{children}</main>
   );
 }
--- src/app/playlists/[playlist-id]/page.tsx
+++ src/app/playlists/[playlist-id]/page.tsx
@@ -1,24 +1,17 @@
-"use client";
-// import { getClient } from "@/lib/apollo-client";
-import PlaylistDetail from "./_components/playlist";
-import { useGetPlaylistQuery } from "@/graphql/hooks";
-// import { GetPlaylistQuery } from "@/graphql/operations";
+import { getClient } from "@/lib/apollo-client";
+import { GetPlaylistDocument } from "@/graphql/hooks";
+import { GetPlaylistQuery } from "@/graphql/operations";
 import NotFound from "@/app/not-found";
-// import ApolloProvider from "@/providers/apollo-provider";
+import ApolloProvider from "@/providers/apollo-provider";
 import { sanitizeData } from "@/lib/sanitize-data";
 import { SongTable } from "../_components/song-table";
-import { useParams } from "next/navigation";
-
+import PlaylistDetail from "./_components/playlist";
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export default function PlaylistDetailPage() {
-  const params = useParams<{ "playlist-id": string }>();
-  const playlistId = params["playlist-id"];
-  // const client = getClient();
-  // const { data, error } = await client.query<GetPlaylistQuery>({
-  //   query: GetPlaylistDocument,
-  //   variables: { id: parseInt(playlistId) },
-  // });
-  const { data, error } = useGetPlaylistQuery({
+export default async function PlaylistDetailPage(props: any) {
+  const playlistId = props.params["playlist-id"];
+  const client = getClient();
+  const { data, error } = await client.query<GetPlaylistQuery>({
+    query: GetPlaylistDocument,
     variables: { id: parseInt(playlistId) },
   });
   const sanitizedData = sanitizeData(data?.playlist.listJson);
@@ -29,14 +22,14 @@ export default function PlaylistDetailPage() {
 
   return (
     <>
-      {/* <ApolloProvider> */}
-      <PlaylistDetail
-        playlistId={playlistId}
-        userId={data?.playlist.userId}
-        playlistData={sanitizedData || []}
-        playlistName={data?.playlist.name || ""}
-      />
-      {/* </ApolloProvider> */}
+      <ApolloProvider>
+        <PlaylistDetail
+          playlistId={playlistId}
+          userId={data?.playlist.userId}
+          playlistData={sanitizedData || []}
+          playlistName={data?.playlist.name}
+        />
+      </ApolloProvider>
       <SongTable songs={sanitizedData || []} />
     </>
   );
