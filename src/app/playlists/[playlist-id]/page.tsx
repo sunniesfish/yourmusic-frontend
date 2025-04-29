a2719c531c29084f254a7d33f71cb89c8cfc6984
sunniesfish <hhch5611.jh@gmail.com>
2025-04-29 20:14:42 +0900

    ssr test

--- src/app/playlists/[playlist-id]/page.tsx
+++ src/app/playlists/[playlist-id]/page.tsx
@@ -1,26 +1,27 @@
-"use client";
 // export const revalidate = 3600;
 
-import { useGetPlaylistQuery } from "@/graphql/hooks";
+import { GetPlaylistDocument, useGetPlaylistQuery } from "@/graphql/hooks";
 import NotFound from "@/app/not-found";
 import ApolloProvider from "@/providers/apollo-provider";
 import { sanitizeData } from "@/lib/sanitize-data";
 import { SongTable } from "../_components/song-table";
 import PlaylistDetail from "./_components/playlist";
 import { useParams } from "next/navigation";
+import { GetPlaylistQuery } from "@/graphql/operations";
+import { getClient } from "@/lib/apollo-client";
 /* eslint-disable @typescript-eslint/no-explicit-any */
-export default function PlaylistDetailPage() {
-  const params = useParams<{ "playlist-id": string }>();
-  const playlistId = params["playlist-id"];
-  const { data, error } = useGetPlaylistQuery({
-    variables: { id: parseInt(playlistId) },
-  });
-  // const playlistId = props.params["playlist-id"];
-  // const client = getClient();
-  // const { data, error } = await client.query<GetPlaylistQuery>({
-  //   query: GetPlaylistDocument,
+export default async function PlaylistDetailPage(props: any) {
+  // const params = useParams<{ "playlist-id": string }>();
+  // const playlistId = params["playlist-id"];
+  // const { data, error } = useGetPlaylistQuery({
   //   variables: { id: parseInt(playlistId) },
   // });
+  const playlistId = props.params["playlist-id"];
+  const client = getClient();
+  const { data, error } = await client.query<GetPlaylistQuery>({
+    query: GetPlaylistDocument,
+    variables: { id: parseInt(playlistId) },
+  });
   const sanitizedData = sanitizeData(data?.playlist.listJson);
 
   if (error) {
